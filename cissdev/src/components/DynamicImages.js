9208d73d1a744ad658e692467c0ab2a2c0dc9d3e
omar cisse <omarcisse97@knights.ucf.edu>
2025-06-22 14:01:55 -0400

    Created util component to dynamically obtain portfolio images

---
+++ cissdev/src/components/DynamicImages.js
@@ -0,0 +1,51 @@
+import CodeByCisseCommerceHomePage from '../assets/codebycisse-commerce/homePage.png';
+import CodeByCisseCommerceCartDrawer from '../assets/codebycisse-commerce/cartDrawer.png';
+import CodeByCisseCommerceCartPage from '../assets/codebycisse-commerce/cartPage.png';
+import CodeByCisseCommerceDarkModeSettings from '../assets/codebycisse-commerce/darkModeSettings.png';
+import CodeByCisseCommerceProductDetails from '../assets/codebycisse-commerce/productDetails.png';
+import CodeByCisseCommerceProfileSettings from '../assets/codebycisse-commerce/profileSettings.png';
+import CodeByCisseCommerceSearchModel from '../assets/codebycisse-commerce/searchModal.png';
+import CodeByCisseCommerceSearchPage from '../assets/codebycisse-commerce/searchPage.png';
+import CodeByCisseRestApiFrameworkLogin from '../assets/codebycisse-restapi-framework/login.png';
+import CodeByCisseRestApiFrameworkDisplayModules from '../assets/codebycisse-restapi-framework/displayModules.png';
+import CodeByCisseRestApiFrameworkApiMenu from '../assets/codebycisse-restapi-framework/apiMenu.png';
+import CodeByCisseRestApiFrameworkGenerateApi from '../assets/codebycisse-restapi-framework/generateAPI.png';
+import CodeByCisseRestApiFrameworkApiActions from '../assets/codebycisse-restapi-framework/apiActions.png';
+import CodeByCisseRestApiFrameworkApiDelete from '../assets/codebycisse-restapi-framework/apiDelete.png';
+import CodeByCisseRestApiFrameworkDatabaseManagement from '../assets/codebycisse-restapi-framework/databaseManagement.png';
+import CodeByCisseRestApiFrameworkTableList from '../assets/codebycisse-restapi-framework/tableList.png';
+import CodeByCisseRestApiFrameworkCreateData from '../assets/codebycisse-restapi-framework/createData.png';
+import CodeByCisseRestApiFrameworkDataDetails from '../assets/codebycisse-restapi-framework/dataDetails.png';
+import CodeByCisseRestApiFrameworkDataEdit from '../assets/codebycisse-restapi-framework/dataEdit.png';
+import CodeByCisseRestApiFrameworkDataReviewDelete from '../assets/codebycisse-restapi-framework/dataReviewDelete.png';
+import CodeByCisseRestApiFrameworkDataDelete from '../assets/codebycisse-restapi-framework/dataDelete.png';
+
+export const ImgProject = () => {
+    return {
+        codeByCisseCommerce: [
+            CodeByCisseCommerceHomePage,
+            CodeByCisseCommerceCartDrawer,
+            CodeByCisseCommerceCartPage,
+            CodeByCisseCommerceDarkModeSettings,
+            CodeByCisseCommerceProductDetails,
+            CodeByCisseCommerceProfileSettings,
+            CodeByCisseCommerceSearchModel,
+            CodeByCisseCommerceSearchPage
+        ],
+        codeByCisseRestApiFramework: [
+            CodeByCisseRestApiFrameworkLogin,
+            CodeByCisseRestApiFrameworkDisplayModules,
+            CodeByCisseRestApiFrameworkApiMenu,
+            CodeByCisseRestApiFrameworkGenerateApi,
+            CodeByCisseRestApiFrameworkApiActions,
+            CodeByCisseRestApiFrameworkApiDelete,
+            CodeByCisseRestApiFrameworkDatabaseManagement,
+            CodeByCisseRestApiFrameworkTableList,
+            CodeByCisseRestApiFrameworkCreateData,
+            CodeByCisseRestApiFrameworkDataDetails,
+            CodeByCisseRestApiFrameworkDataEdit,
+            CodeByCisseRestApiFrameworkDataReviewDelete,
+            CodeByCisseRestApiFrameworkDataDelete
+        ]
+    }
+}
